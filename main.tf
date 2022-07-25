61e3db65f0fec79d221a52ce89e4d2fdf4de80a1
Casey Wood <casey.wood@cloudreach.com>
2022-07-25 13:03:32 -0700

    update

--- main.tf
+++ main.tf
@@ -4,3 +4,42 @@ resource "azurerm_key_vault_secret" "ixmessaging_password" {
   key_vault_id = data.azurerm_key_vault.customer_keyvault.id
 }
 
+resource "azurerm_windows_virtual_machine_scale_set" "ixmessaging" {
+  name                = "${var.customername}-ixm"
+  resource_group_name = data.azurerm_resource_group.rg_uc_apps.name
+  location            = var.location
+  sku                 = "Standard_D8s_v4"
+  admin_username      = "adminuser"
+  admin_password      = azurerm_key_vault_secret.ixmessaging_password.value
+  license_type = "Windows_Server"
+  instances           = 3
+  enable_automatic_updates = true
+  tags = var.tags
+  #source_image_id = ""
+
+
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    managed_disk_type = "Standard_LRS"
+    caching              = "ReadWrite"
+    
+  }
+
+  network_interface {
+    name    = "internalinterface"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = data.azurerm_virtual_network.corespoke_vnet.subnet[2].id
+    }
+  }
+}
