23da2a4f786d2fff79f085c9577c941df7d97b30
Casey Wood <casey.wood@cloudreach.com>
2022-07-25 12:01:22 -0700

    update

--- provider.tf
+++ provider.tf
@@ -10,21 +10,36 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.10.0"
+      version = ">=3.15.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.3.1"
+      version = ">=3.3.2"
     }
 
   }
 }
 
-# provider "azurerm" {
-#   features {}
-#   disable_correlation_request_id = true
-# }
+provider "azurerm" {
+  features {}
+  subscription_id = "53eddfd8-cb7b-4235-8d0c-188371c1debc"
+  disable_correlation_request_id = true
+}
 
-# provider "random" {
 
-# }
+provider "random" {
+
+}
+
+data "azurerm_resource_group" "sharedservices_vnet_resourcegroup" {
+  provider = azurerm.sharedservices
+  name     = "rg-vnet-shared-services-eastus2"
+}
+
+data "azurerm_virtual_network" "sharedservices_vnet" {
+  provider            = azurerm.sharedservices
+  name                = "vnet-sharedservices-eastus2"
+  resource_group_name = data.azurerm_resource_group.sharedservices_vnet_resourcegroup.name
+
+
+}
