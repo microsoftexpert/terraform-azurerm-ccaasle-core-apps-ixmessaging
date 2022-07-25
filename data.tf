8da5f25a182ba0a93313951fffd850b75ec9ba3a
Casey Wood <casey.wood@cloudreach.com>
2022-07-25 12:23:51 -0700

    update

--- data.tf
+++ data.tf
@@ -1,3 +1,12 @@
+resource "random_password" "ixmessaging_password" {
+  length      = 15
+  special     = true
+  min_upper   = 3
+  min_lower   = 3
+  min_special = 3
+  min_numeric = 3
+}
+
 
 data "azurerm_resource_group" "rg_hub_vnet" {
   name     = "rg-vnet-shared-services-eastus2"
@@ -15,6 +24,10 @@ data "azurerm_resource_group" "rg_uc_apps" {
   name     = "rg-vms-uc-apps-eastus2-DEV1"
 }
 
+data "azurerm_resource_group" "rg_coreservices" {
+  name     = "rg-coreservices-${var.location}-${var.customername}"
+}
+
 data "azurerm_virtual_network" "hub_vnet" {
   name                = "vnet-hub-eastus2-DEV1"
   resource_group_name = data.azurerm_resource_group.rg_hub_vnet.name
@@ -23,4 +36,9 @@ data "azurerm_virtual_network" "hub_vnet" {
 data "azurerm_virtual_network" "corespoke_vnet" {
   name                = "vnet-core-eastus2-DEV1"
   resource_group_name = data.azurerm_resource_group.rg_hub_vnet.name
+}
+
+data "azurerm_key_vault" "customer_keyvault" {
+  name = "kv-${var.customername}-${var.location}"
+  resource_group_name = data.azurerm_resource.group.rg_coreservices.name
 }
